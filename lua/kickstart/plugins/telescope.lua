59b4a83b3f1016e1ae15eb643781db2bf91a31b0
Kacper Soczko <kacper.soczko@gmail.com>
2025-07-11 17:54:21 +0200

    add lazygit

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -30,27 +30,16 @@ return {
     },
     config = function()
       require('telescope').setup {
-        -- You can put your default mappings / updates / etc. in here
-        --  All the info you're looking for is in `:help telescope.setup()`
-        --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
-        -- pickers = {}
         extensions = {
-          ['ui-select'] = {
-            require('telescope.themes').get_dropdown(),
-          },
           filebrowser = {
             hijack_netrw = true,
-            initial_mode = 'normal',
+          },
+          ['ui-select'] = {
+            require('telescope.themes').get_dropdown(),
           },
         },
       }
 
-      -- Enable Telescope extensions if they are installed
       pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
       pcall(require('telescope').load_extension, 'file_browser')
